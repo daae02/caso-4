3d952d7d8bce7aa3953ef920b49d384756d8314e
Sebastián Díaz Obando <sebascout01182@gmail.com>
2021-03-15 16:21:01 -0600

    Ideas base para el caso#4 y el archivo main

---
+++ Caso#4.cpp
@@ -0,0 +1,17 @@
+//Codigo base
+//Funcion:
+//Parametro : tamaño de la matriz , largo por ancho
+//Crear una matriz con los parametros asignados que por defecto toda la matriz sea de color blanco(metodo fill)
+//Condicion: if es par: llenar toda la linea con el color gris
+//Else: de manera aleatoria llenar la posiscion con un color aleatorio entre el gris y el negro
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
